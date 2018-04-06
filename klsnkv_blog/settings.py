ef2a0b64b87213879e93960572e9f59a73c7bd8e
klsnkv <klsnkv@klsnkvs-MacBook-Pro.local>
2018-04-06 12:56:24 +0200

    added superuser and posts model

--- klsnkv_blog/settings.py
+++ klsnkv_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'klsnkv_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'klsnkv_blog_db',
+        'USER': 'klsnkv',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
